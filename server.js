0537d2d8fa56e3f5444cc34334f575df511458af
Kerja9999999 <106123557+Kerja9999999@users.noreply.github.com>
2026-06-30 20:58:36 +0300

    Update server.js

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.post(
 // ---------- Awora ----------
 setInterval(async () => {
     await checkOrders(sendTelegram);
-}, 305000);
+}, 805000);
 
 // ---------- Test ----------
 app.get("/", (_, res) => res.send("Bot is running"));
