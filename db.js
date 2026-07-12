7fe5e29c8aaa4cf33056552e5dab4be197663dea
Kerja9999999 <106123557+Kerja9999999@users.noreply.github.com>
2026-07-12 13:57:56 +0300

    Delete db.js

--- db.js
+++
@@ -1,8 +0,0 @@
-const { createClient } = require("@libsql/client");
-
-const db = createClient({
-    url: process.env.TURSO_DATABASE_URL,
-    authToken: process.env.TURSO_AUTH_TOKEN
-});
-
-module.exports = db;
